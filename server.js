27c0359c1d423f7db18f78d67f910ae2f47698e0
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-11-30 11:28:07 +1000

    Updated Server Port

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.get('/api/pets', (req, res) => {
   res.send(pets)
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3001, () => console.log('Example app listening on port 3000!'))
