f8acb57dfbffd84ed46408967b796864be8fb4b8
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-11-30 11:01:37 +1000

    Express installed & created GET /api/pets endpoint

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Server!!!');
+const express = require('express')
+const app = express()
+
+const pets = ['Kitten', 'Cat', 'Pupper', 'Doggo']
+
+app.get('/api/pets', (req, res) => {
+  res.send(pets)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
